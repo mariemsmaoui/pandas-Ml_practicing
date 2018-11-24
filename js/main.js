0d50c52f6b2e767cade524dfce5014c53f54fc7e
mariemsmaoui <sm.mariem99@gmail.com>
2018-11-24 18:00:47 +0000

    ...

---
+++ js/main.js
@@ -0,0 +1,25 @@
+function bold() {
+  let g =document.getElementById("mimi");
+  if (g.style.fontWeight=="normal")
+  g.style.fontWeight = "bold"
+else g.style.fontWeight="normal"
+}
+function underlined() {
+let s=document.getElementById("mimi");
+if (s.style.fontWeight=="normal")
+  s.style.fontWeight = "underlined"
+else s.style.fontWeight="normal"
+}
+function italic() {
+  let a=document.getElementById("mimi");
+  if (a.style.fontWeight=="normal")
+    a.style.fontWeight = "italic"
+  else a.style.fontWeight="normal"
+  }
+  function arial() {
+    let q=document.getElementById("mimi");
+    if (q.style.fontWeight=="normal")
+      q.style.fontWeight = "arial"
+    else q.style.fontWeight="normal"
+    }
+      
